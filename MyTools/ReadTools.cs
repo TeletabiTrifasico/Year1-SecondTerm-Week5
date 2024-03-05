28db1ff7026808aebfa2fffbae411844b0cc88a1
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 19:32:49 +0100

    Add files via upload

---
+++ MyTools/ReadTools.cs
@@ -0,0 +1,29 @@
+﻿namespace MyTools
+{
+    public class ReadTools
+    {
+        public static int ReadInt(string question)
+        {
+            Console.Write(question);
+            int value = Convert.ToInt32(Console.ReadLine());
+            return value;
+        }
+        public static int ReadInt(string question, int min, int max)
+        {
+            Console.Write(question);
+            int value = Convert.ToInt32(Console.ReadLine());
+            while (value < min || value > max)
+            {
+                Console.Write(question);
+                value = Convert.ToInt32(Console.ReadLine());
+            }
+            return value;
+        }
+        public static string ReadString(string question)
+        {
+            Console.Write(question);
+            string value = Console.ReadLine();
+            return value;
+        }
+    }
+}

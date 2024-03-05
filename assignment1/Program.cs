437759b1d040bc32fcd5c95e1efa6ad5f170b066
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 19:28:58 +0100

    Add files via upload

---
+++ assignment1/Program.cs
@@ -0,0 +1,26 @@
+﻿using MyTools;
+
+namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+        void Start()
+        {
+            int value = ReadTools.ReadInt("Enter a value: ");
+            Console.WriteLine("You entered {0}.", value);
+
+            int age = ReadTools.ReadInt("How old are you? ", 0, 120);
+            Console.WriteLine("You are {0} years old.", age);
+
+            string name = ReadTools.ReadString("What is your name? ");
+            Console.WriteLine("Nice meeting you {0}.", name);
+
+            Console.ReadKey();
+        }
+    }
+}
